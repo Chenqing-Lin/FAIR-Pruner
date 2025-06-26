3296eb0052fd5d7ce8e860142252554c1c8e403b
Chenqing-Lin <cq_lin9912@163.com>
2025-06-26 11:29:25 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Network_Pruner',
+    version='0.5',
+    author='Chenqing Lin',
+    author_email='24020040061@pop.zjgsu.edu.cn',
+    install_requires=[
+
+    ]
+)
