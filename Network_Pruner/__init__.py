2be0270693167e7cd0464c915b357c3153c0697c
Chenqing-Lin <cq_lin9912@163.com>
2025-06-27 16:51:53 +0800

    Update

--- Network_Pruner/__init__.py
+++ Network_Pruner/__init__.py
@@ -1,4 +1,2 @@
 # Network_pruner/__init__.py
 #
-import torch.nn as nn
-import torch
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Network_Pruner',
-    version='0.6',
+    version='0.7',
     author='Chenqing Lin',
     author_email='24020040061@pop.zjgsu.edu.cn',
     packages = ["Network_Pruner"],
